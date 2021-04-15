4963c73e40dc3e23a0380bd4f82461cb764c747f
bernardd0 <vitor.bernardo@yahoo.com.br>
2021-04-15 16:12:03 -0300

    feat: back-end finished

--- server.js
+++ server.js
@@ -2,41 +2,15 @@
 const express = require("express")
 const server = express()
 
-const ideas = [
-    {
-        img: "https://www.flaticon.com/svg/vstatic/svg/2737/2737110.svg?token=exp=1618229452~hmac=f68907138362c41a0bdddbb66e25beff",
-        title: "Curso de Programacão",
-        category: "Estudo",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus magni eum quod vel cupiditate,",
-        url: "https://github.com"
-    },
-    {
-        img: "https://www.flaticon.com/svg/vstatic/svg/2737/2737379.svg?token=exp=1618229396~hmac=1861680c0eb73b531b6b9d9f90535c96",
-        title: "Videogame",
-        category: "Diversao",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus magni eum quod vel cupiditate,",
-        url: "https://github.com"
-    },
-    {
-        img: "https://www.flaticon.com/svg/vstatic/svg/3081/3081474.svg?token=exp=1618229579~hmac=b68269aa6abe329a86944d0c51a4f795",
-        title: "Meditacão",
-        category: "Mentalidade",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus magni eum quod vel cupiditate,",
-        url: "https://github.com"
-    },
-    {
-        img: "https://www.flaticon.com/svg/vstatic/svg/2933/2933488.svg?token=exp=1618264584~hmac=cc6bb9ff85b317908a48b3a40c435c08",
-        title: "Leitura",
-        category: "Educação",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus magni eum quod vel cupiditate,",
-        url: "https://github.com"
-    },
-]
+const db = require("./db")
 
-//Set up static files (css, scripts, images)
+//Set up static files / Configurar arquivos estáticos (css, scripts, images)
 server.use(express.static("public"))
 
-//Set up nunjucks
+//Set up use of req.body / Habilita o uso do req.body
+server.use(express.urlencoded({extended: true}))
+
+//Set up nunjucks / Configuração do nunjucks
 const nunjucks = require("nunjucks")
 nunjucks.configure("views", {
     express: server,
@@ -44,19 +18,55 @@ nunjucks.configure("views", {
 })
 
 //Router created | Client's request
-server.get("/home", function(req, res) {
-    const lastIdeas = []
-    for (idea of ideas) {
-        if (lastIdeas.length < 2) {
-            lastIdeas.push(idea)
+server.get("/", function(req, res) {
+    //Info: Req has a property callde Query that pulls the data from the URL 
+    //req.query
+    //Verify data in the table
+    db.all(`SELECT * FROM ideas`, function(err, rows){
+        if (err) return console.log(err)
+        const lastIdeas = []
+        for (idea of rows) {
+            if (lastIdeas.length < 2) {
+                lastIdeas.push(idea)
+            }
         }
-    }
-    return res.render("index.html", {ideas: lastIdeas})
+        return res.render("index.html", {ideas: lastIdeas})
+    })
 })
 
 server.get("/ideias", function(req, res) {
-    return res.render("ideias.html", {ideas})
+    db.all(`SELECT * FROM ideas`, function(err, rows){
+        const ideas = rows
+        if (err) return console.log(err)
+        return res.render("ideias.html", {ideas})
+    })
 })
 
+//
+server.post("/", function(req, res) {
+    //Insert data in the table
+    const query = `
+        INSERT INTO ideas(
+            image,
+            title,
+            category,
+            description,
+            link
+        ) VALUES (?,?,?,?,?);
+    `
+    const values =  [
+        req.body.image,
+        req.body.title,
+        req.body.category,
+        req.body.description,
+        req.body.link,
+    ]
+    db.run(query, values, function(err) {
+        console.log(req.body)
+    //Verify data in the table
+        if (err) return console.log(err) 
+        return res.redirect("/ideias")
+    })
+})
 // Server started
 server.listen(3000)
